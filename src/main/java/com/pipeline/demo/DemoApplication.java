4e96c9e88d7f69c2e2ded30933846862ce458680
ahmedmohsen08 <ahmedmohsen0888@gmail.com>
2024-03-26 14:24:41 +0200

    add get api

--- src/main/java/com/pipeline/demo/DemoApplication.java
+++ src/main/java/com/pipeline/demo/DemoApplication.java
@@ -2,12 +2,22 @@ package com.pipeline.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@GetMapping("/api")
+	public String getMethodName(@RequestParam String param) {
+		return new String("hello");
+	}
+	
 }
